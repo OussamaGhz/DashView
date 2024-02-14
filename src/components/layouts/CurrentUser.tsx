950a3f31351b0f8b9900538b84b8c42bb55e0d85
SohaibGhz <sohaib.ghazi@outlook.fr>
2024-02-14 21:18:29 +0100

    running codegen

--- src/components/layouts/CurrentUser.tsx
+++ src/components/layouts/CurrentUser.tsx
@@ -8,8 +8,11 @@ import { useGetIdentity } from "@refinedev/core";
   1. must add the plugin on vite config (vite.config.ts)
   2. must add the schema.types.ts on graphql config (graphql.config.ts)
   3. define the base url and the paths on ts config (tsconfig.ts)
+  4. add code gen script on package file (package.json)
+  5. create mutations and queries (see graphql folder)
 */
 
+// importing the User type form graphql schema
 import type { User } from "@/graphql/schema.types";
 
 const CurrentUser = () => {
